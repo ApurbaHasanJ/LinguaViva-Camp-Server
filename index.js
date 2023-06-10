ab7c38498c3f2bd1bd7be362e4937315b5a2a9fe
ApurbaHasanJ <apurbahasanj@gmail.com>
2023-06-10 13:19:56 +0600

    added JWT

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.port || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// const email = req.query.email;
+//       if(!email){
+//         res.send([])
+//       }
+//       const decodedEmail = req.decoded.email
+//       if(email !== decodedEmail){
+//         return res.send(403).send({error: true, message : 'forbidden access'})
+//       }
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@believer.igrxpib.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,8 +57,17 @@ async function run() {
 
     const usersCollection = client.db("LVCdb").collection("users");
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const userToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ userToken });
+    });
+
     // get all users
     app.get("/users", async (req, res) => {
+      
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
