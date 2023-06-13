1ae5ed620c8f5cbe90590f24ae6a9bf4c75bf4f3
ApurbaHasanJ <apurbahasanj@gmail.com>
2023-06-14 01:30:27 +0600

    deploy complete

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("LVCdb").collection("users");
     const classesCollection = client.db("LVCdb").collection("classes");
